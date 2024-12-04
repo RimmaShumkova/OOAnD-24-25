b1c1ff347be79f01b3299103d6e4727f9838cd75
Rimma Shumkova <rimma.shumkova.05@mail.ru>
2024-12-04 14:18:55 +0600

    fix: added VectorTests

--- SpaceBattle.Tests/MovingTests.cs
+++ SpaceBattle.Tests/MovingTests.cs
@@ -63,3 +63,119 @@ public class MovingTests
         Assert.Throws<ArgumentException>(act);
     }
 }
+public class VectorTests
+{
+    [Fact]
+    public void VectorIsInitializableTest()
+    {
+        int[] elements = { 1, 2, 3 };
+        var vector = new Vector(elements);
+        Assert.True(elements.SequenceEqual(vector.GetElements()));
+    }
+
+    [Fact]
+    public void VectorEqualsReturnsTrueForIdenticalVectorsTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(1, 2, 3);
+        Assert.True(v1.Equals(v2));
+    }
+
+    [Fact]
+    public void VectorEqualsReturnsFalseForDifferentVectorsTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(4, 5, 6);
+        Assert.False(v1.Equals(v2));
+    }
+
+    [Fact]
+    public void VectorEqualsReturnsFalseForNullTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        Assert.False(v1.Equals(null));
+    }
+
+    [Fact]
+    public void VectorEqualsReturnsFalseForDifferentTypeTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        var obj = new object();
+        Assert.False(v1.Equals(obj));
+    }
+
+    [Fact]
+    public void VectorOperatorPlusAddsVectorsCorrectlyTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(4, 5, 6);
+        var result = v1 + v2;
+        var expected = new Vector(5, 7, 9);
+        Assert.True(result.Equals(expected));
+    }
+
+    [Fact]
+    public void VectorOperatorPlusThrowsForDifferentLengthsTest()
+    {
+        var v1 = new Vector(1, 2);
+        var v2 = new Vector(3, 4, 5);
+        Assert.Throws<InvalidOperationException>(() => _ = v1 + v2);
+    }
+
+    [Fact]
+    public void VectorOperatorEqualsReturnsTrueForSameReferenceTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        var v2 = v1;
+        Assert.True(v1 == v2);
+    }
+
+    [Fact]
+    public void VectorOperatorNotEqualsReturnsTrueForDifferentVectorsTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(4, 5, 6);
+        Assert.True(v1 != v2);
+    }
+
+    [Fact]
+    public void VectorGetHashCodeReturnsSameForIdenticalVectorsTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(1, 2, 3);
+        var hash1 = v1.GetHashCode();
+        var hash2 = v2.GetHashCode();
+        Assert.Equal(hash1, hash2);
+    }
+
+    [Fact]
+    public void VectorGetHashCodeReturnsDifferentForDifferentVectorsTest()
+    {
+        var v1 = new Vector(1, 2, 3);
+        var v2 = new Vector(4, 5, 6);
+        var hash1 = v1.GetHashCode();
+        var hash2 = v2.GetHashCode();
+        Assert.NotEqual(hash1, hash2);
+    }
+
+    [Fact]
+    public void ToStringReturnsCorrectStringRepresentation()
+    {
+        var vector = new Vector(1, 2, 3);
+        var result = vector.ToString();
+        Assert.Equal("Vector (1, 2, 3)", result);
+    }
+
+    [Fact]
+    public void IndexerReturnsCorrectElement()
+    {
+        var vector = new Vector(1, 2, 3);
+        Assert.Equal(2, vector[1]);
+    }
+
+    [Fact]
+    public void ConstructorThrowsForEmptyVector()
+    {
+        Assert.Throws<ArgumentException>(() => new Vector());
+    }
+}
