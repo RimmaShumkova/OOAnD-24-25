cc2740e17fdd889548f8623ad474d15db6127697
Rimma Shumkova <rimma.shumkova.05@mail.ru>
2024-12-24 19:56:30 +0600

    added: MacroCommand, MacroCommandTests, RegisterIoCDependencyMacroCommand, MacroCommandRegisterTests

---
+++ SpaceBattle.Tests/MacroCommandRegisterTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using App;
+using App.Scopes;
+
+namespace SpaceBattle.Lib
+{
+    public class MacroCommandRegisterTests
+    {
+        [Fact]
+        public void TestExecute()
+        {
+            new InitCommand().Execute();
+
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var cmd1 = new Mock<ICommand>();
+            var cmd2 = new Mock<ICommand>();
+            var cmd3 = new Mock<ICommand>();
+
+            var registerMacroCommand = new RegisterIoCDependencyMacroCommand();
+            registerMacroCommand.Execute();
+
+            ICommand[] arr = { cmd1.Object, cmd2.Object, cmd3.Object };
+            var macroCommand = Ioc.Resolve<ICommand>("Commands.Macro", arr);
+
+            Assert.IsType<MacroCommand>(macroCommand);
+        }
+    }
+}
