d415a2ecb0bb89c010f78140d5c7d6d160044f75
AvdushkinaKsenia <ksukenka@gmail.com>
2024-12-24 19:46:10 +0600

    feat: added RegisterDependencyCommandInjectableCommand

---
+++ SpaceBattle.Tests/RegisterDependencyCommandInjectableCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using App;
+using App.Scopes;
+namespace SpaceBattle.Lib.Tests;
+
+public class RegisterDependencyCommandInjectableCommandTests
+{
+    [Fact]
+    public void RegisterTest()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+        var registerCm = new RegisterDependencyCommandInjectableCommand();
+        registerCm.Execute();
+
+        Ioc.Resolve<ICommand>("Commands.CommadInjectable");
+        Ioc.Resolve<ICommandInjectable>("Commands.CommadInjectable");
+        Ioc.Resolve<InjectableCommand>("Commands.CommadInjectable");
+
+    }
+}
