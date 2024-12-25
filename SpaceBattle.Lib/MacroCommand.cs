abda0b749f0e39fc838f6c8516ee22394e249527
Rimma Shumkova <rimma.shumkova.05@mail.ru>
2024-12-25 11:26:01 +0600

    added: CreateMacroCommandStrategy, CreateMacroCommandStrategyTests

---
+++ SpaceBattle.Lib/MacroCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle.Lib;
+
+public class MacroCommand : ICommand
+{
+    private readonly ICommand[] cmds;
+    public MacroCommand(ICommand[] cmds)
+    {
+        this.cmds = cmds;
+    }
+
+    public void Execute()
+    {
+        var commandList = new List<ICommand>();
+        commandList = cmds.ToList<ICommand>();
+        commandList.ForEach(c => c.Execute());
+    }
+}
