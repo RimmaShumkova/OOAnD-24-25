57f6f73c0c7582131de4451bdceac9af8fdff2da
AvdushkinaKsenia <ksukenka@gmail.com>
2025-03-18 22:03:26 +0600

    feat: added Execute()

--- SpaceBattle.Lib/ShootCommand.cs
+++ SpaceBattle.Lib/ShootCommand.cs
@@ -16,8 +16,6 @@ public class ShootCommand : ICommand
         
         var initialProperties = Ioc.Resolve<IDictionary<string, object>>("Game.Projectile.Properties", shooter);
         
-        var moveCommand = Ioc.Resolve<ICommand>("Commands.Move", initialProperties);
-        
-        Ioc.Resolve<ICommand>("Commands.Macro.Create", new[] { moveCommand }).Execute();
+        Ioc.Resolve<ICommand>("Commands.Move", initialProperties).Execute();
     }
 }
