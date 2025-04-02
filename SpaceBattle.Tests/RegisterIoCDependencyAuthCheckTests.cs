5f05a83307d2243768c44357bc4b119ecb729d57
AvdushkinaKsenia <ksukenka@gmail.com>
2025-04-02 08:49:43 +0600

    fix: style

--- SpaceBattle.Tests/RegisterIoCDependencyAuthCheckTests.cs
+++ SpaceBattle.Tests/RegisterIoCDependencyAuthCheckTests.cs
@@ -1,4 +1,4 @@
-using App;
+﻿using App;
 using App.Scopes;
 
 namespace SpaceBattle.Lib.Tests
@@ -9,7 +9,7 @@ namespace SpaceBattle.Lib.Tests
         {
             new InitCommand().Execute();
             var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
-            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute(); 
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
         }
 
         [Fact]
