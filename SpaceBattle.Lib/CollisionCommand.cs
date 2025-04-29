4cc8dabd57b93ce4bae8db9a6e20d8ce44c93fb2
Yana1606 <istominayana20@gmail.com>
2025-04-29 23:21:05 +0600

    feat: added CollisionCommand

---
+++ SpaceBattle.Lib/CollisionCommand.cs
@@ -0,0 +1,30 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+
+public class CollisionCommand : ICommand
+{
+    private readonly IColliding firstObj;
+
+    private readonly IColliding secondObj;
+
+    private readonly ICommand command;
+
+    public CollisionCommand(IColliding firstObj, IColliding secondObj, ICommand command)
+    {
+        this.firstObj = firstObj;
+        this.secondObj = secondObj;
+        this.command = command;
+    }
+    public void Execute()
+    {
+        var deltaPosition = Ioc.Resolve<Array>("Game.GetVectorDifference", firstObj.Position, secondObj.Position);
+
+        var deltaVelocity = Ioc.Resolve<Array>("Game.GetVectorDifference", firstObj.Velocity, secondObj.Velocity);
+
+        var isCollision = Ioc.Resolve<bool>("Game.IsCollision", deltaPosition, deltaVelocity, firstObj.Shape, secondObj.Shape);
+        if (isCollision)
+        {
+            command.Execute();
+        }
+    }
+}
