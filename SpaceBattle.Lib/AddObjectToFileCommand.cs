d41d94b2c5533ddad67a445e6c39b5c3e40ef64f
Yana1606 <istominayana20@gmail.com>
2025-04-30 09:21:13 +0600

    feat: added AddObjectToFileCommand

---
+++ SpaceBattle.Lib/AddObjectToFileCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+namespace SpaceBattle.Lib;
+
+public class AddObjectToFileCommand : ICommand
+{
+    private readonly object objectToAdd;
+    private readonly string filePath;
+
+    public AddObjectToFileCommand(string filePath, object objectToAdd)
+    {
+        this.objectToAdd = objectToAdd;
+        this.filePath = filePath;
+    }
+
+    public void Execute()
+    {
+        var jsonString = JsonSerializer.Serialize(objectToAdd);
+        File.WriteAllText(filePath, jsonString);
+    }
+}
