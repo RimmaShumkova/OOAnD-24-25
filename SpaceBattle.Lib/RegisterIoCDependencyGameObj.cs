8ae82f32a77446d4e5e7bf4a9abb89f41a4f46a7
Rimma Shumkova <rimma.shumkova.05@mail.ru>
2025-05-12 23:27:18 +0600

    added: RegisterIoCDependencyGameObj

---
+++ SpaceBattle.Lib/RegisterIoCDependencyGameObj.cs
@@ -0,0 +1,43 @@
+﻿namespace SpaceBattle.Lib;
+using App;
+
+public class RegisterIoCDependencyGameObj : ICommand
+{
+    public void Execute()
+    {
+        var gameObj = new Dictionary<string, Dictionary<string, object>>();
+
+        Ioc.Resolve<ICommand>(
+            "Ioc.Register",
+            "GameObject.Add",
+            (object[] args) => new AddGameObjectCommand(
+                gameObj,
+                (Dictionary<string, object>)args[0]
+            )
+        ).Execute();
+
+        Ioc.Resolve<ICommand>(
+            "Ioc.Register",
+            "GameObject.Remove",
+            (object[] args) => new RemoveGameObjectCommand(
+                gameObj,
+                (string)args[0]
+            )
+        ).Execute();
+
+        Ioc.Resolve<ICommand>(
+            "Ioc.Register",
+            "GameObject.Get",
+            (object[] args) =>
+            {
+                var itemId = (string)args[0];
+                if (!gameObj.TryGetValue(itemId, out var result))
+                {
+                    throw new Exception($"GameObject с '{itemId}' не найден.");
+                }
+
+                return result;
+            }
+        ).Execute();
+    }
+}
