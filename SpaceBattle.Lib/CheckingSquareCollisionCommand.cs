4c4e874308b94f9f82f8acae30f8153ca2f0988a
Rimma Shumkova <rimma.shumkova.05@mail.ru>
2025-05-13 22:55:16 +0600

    feat: added CheckingSquareCollisionCommand

---
+++ SpaceBattle.Lib/CheckingSquareCollisionCommand.cs
@@ -0,0 +1,23 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class CheckingSquareCollisionCommand : ICommand
+    {
+        private readonly IColliding collObj;
+
+        public CheckingSquareCollisionCommand(IColliding collObj)
+        {
+            this.collObj = collObj;
+        }
+
+        public void Execute()
+        {
+            var neighborObj = Ioc.Resolve<IEnumerable<IColliding>>("Game.GetObjectsInSameSquare", collObj);
+            neighborObj
+                .Select(obj => Ioc.Resolve<ICommand>("Game.CollisionCommand", collObj, obj, Ioc.Resolve<ICommand>("Game.HandleCollision", collObj, obj)))
+                .ToList()
+                .ForEach(cmd => cmd.Execute());
+        }
+    }
+}
