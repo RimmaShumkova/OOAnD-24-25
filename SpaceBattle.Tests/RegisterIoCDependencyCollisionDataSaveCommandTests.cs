1f836473405d34942e10b5e4ec822f2bfef51d0a
AvdushkinaKsenia <ksukenka@gmail.com>
2025-05-14 09:17:02 +0600

    feat: added RegisterIoCDependencyCollisionDataSaveCommandTests

---
+++ SpaceBattle.Tests/RegisterIoCDependencyCollisionDataSaveCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using App;
+using App.Scopes;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyCollisionDataSaveCommandTests
+{
+    public RegisterIoCDependencyCollisionDataSaveCommandTests()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+
+    [Fact]
+    public void ExecuteTestCollisionSaveDataCommand()
+    {
+        var collisionName = "testCollision";
+        var collisionData = new List<int[]> { new int[] { 1, 2 }, new int[] { 3, 4 } };
+
+        var registerCommand = new RegisterIoCDependencyCollisionDataSaveCommand();
+
+        registerCommand.Execute();
+
+        var resolvedCommand = Ioc.Resolve<ICommand>(
+            "Collision.SaveDataCommand",
+            collisionName,
+            collisionData
+        );
+
+        Assert.IsType<CollisionSaveDataCommand>(resolvedCommand);
+    }
+}
