fe2c6564818ae5c13fc6c63bbf361603393f6e49
Rimma Shumkova <rimma.shumkova.05@mail.ru>
2025-05-14 14:24:21 +0600

    added: RegisterIoCDependencyCollisionPrepareDataTests

---
+++ SpaceBattle.Tests/RegisterIoCDependencyCollisionPrepareDataTests.cs
@@ -0,0 +1,47 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class RegisterIoCDependencyCollisionPrepareDataTests
+    {
+        public RegisterIoCDependencyCollisionPrepareDataTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void Execute_RegistersCollisionPrepareDataDependency()
+        {
+            var mockDataGenerator = new Mock<ICollisionDataGenerator>();
+            var regCommand = new RegisterIoCDependencyCollisionPrepareData();
+
+            regCommand.Execute();
+
+            var collPrepareDataCommand = Ioc.Resolve<ICommand>("Collision.PrepareData", mockDataGenerator.Object);
+            Assert.NotNull(collPrepareDataCommand);
+            Assert.IsType<CollisionPrepareDataCommand>(collPrepareDataCommand);
+        }
+
+        [Fact]
+        public void Execute_RegistersDependencyCorrectlyWithDifferentArguments()
+        {
+            var mockDataGenerator1 = new Mock<ICollisionDataGenerator>();
+            var mockDataGenerator2 = new Mock<ICollisionDataGenerator>();
+            var regCommand = new RegisterIoCDependencyCollisionPrepareData();
+
+            regCommand.Execute();
+
+            var collPrepareDataCommand1 = Ioc.Resolve<ICommand>("Collision.PrepareData", mockDataGenerator1.Object);
+            var collPrepareDataCommand2 = Ioc.Resolve<ICommand>("Collision.PrepareData", mockDataGenerator2.Object);
+
+            Assert.NotNull(collPrepareDataCommand1);
+            Assert.NotNull(collPrepareDataCommand2);
+            Assert.IsType<CollisionPrepareDataCommand>(collPrepareDataCommand1);
+            Assert.IsType<CollisionPrepareDataCommand>(collPrepareDataCommand2);
+        }
+    }
+}
